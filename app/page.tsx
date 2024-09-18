735a99f24c5b4baec83c2fa516806bfa4e7f98d6
legalito <63124952+legalito@users.noreply.github.com>
2024-09-18 10:55:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Hero from '../components/Hero/hero';
 import Popular from '../components/Popular/popular';
 import Release from '../components/Releases/release';
-import FindPerGenre from '../components/FindPerGenre/findPerGenre';
+import FindPerGenre from '../components/FindPerGenre/findpergenre';
 export default function Home() {
   return (
     <main>
