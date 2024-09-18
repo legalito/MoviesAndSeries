6ad87a520919fa12a40a2bfa99a65ccddbf5c516
legalito <63124952+legalito@users.noreply.github.com>
2024-09-18 10:56:47 +0200

    Update page.js

--- app/tvshows/page.js
+++ app/tvshows/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Hero from '../../components/Hero/hero';
 import Popular from '../../components/Popular/popular';
 import Discover from '../../components/Discover/discover';
-import FindPerGenre from '../../components/FindPerGenre/findPerGenre';
+import FindPerGenre from '../../components/FindPerGenre/findpergenre';
 export default function Home() {
   return (
     <main>
