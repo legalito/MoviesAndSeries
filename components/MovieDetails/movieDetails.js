408ef2615c52d751dc0f53e970d121f38584e688
legalito <63124952+legalito@users.noreply.github.com>
2024-09-18 15:44:34 +0200

    Update movieDetails.js

--- components/MovieDetails/movieDetails.js
+++ components/MovieDetails/movieDetails.js
@@ -5,6 +5,8 @@ import styles from "./movieDetails.module.css";
 
 export default function MovieDetails() {
   const [moviesData, setMoviesData] = useState([]);
+  const [idMovie, setIdMovie] = useState(null); // Ajout d'un état pour stocker l'idMovie
+
   const options = {
     method: "GET",
     headers: {
@@ -14,99 +16,113 @@ export default function MovieDetails() {
     },
   };
 
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const idMovie = urlParams.get("idMovie");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // On vérifie que window est défini
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const id = urlParams.get("idMovie");
+      setIdMovie(id); // On met à jour l'état avec l'idMovie
+    }
+  }, []);
 
   useEffect(() => {
-    console.log("idMovie", idMovie);
-    const fetchData = async () => {
-      try {
-        const trendingResponse = await axios.get(
-          `https://api.themoviedb.org/3/movie/${idMovie}`,
-          options
-        );
-        const additionnalDataVideo = await axios.get(
-          `https://api.themoviedb.org/3/movie/${idMovie}/videos`,
-          options
-        );
-        const detailsResponse = await axios.get(
-          `https://api.themoviedb.org/3/movie/${idMovie}/images`,
-          options
-        );
-        console.log(
-          "additionnalDataVideo",
-          additionnalDataVideo.data.results[0].key
-        );
-        const responseIds = trendingResponse.data;
-        const finalData = {
-          id: responseIds.id,
-          title: responseIds.title,
-          original_title: responseIds.original_title,
-          backdrop_path: responseIds.backdrop_path,
-          overview: responseIds.overview,
-          video: additionnalDataVideo.data.results[0].key,
-          genres : responseIds.genres,
-          note : responseIds.vote_average,
-          images : detailsResponse.data.backdrops,
-        };
-        console.log("finalData", finalData);
-        setMoviesData([finalData]);
-        document.body.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${finalData.backdrop_path}`;
+    if (idMovie) {
+      // On ne lance la requête que si idMovie est défini
+      const fetchData = async () => {
+        try {
+          const trendingResponse = await axios.get(
+            `https://api.themoviedb.org/3/movie/${idMovie}`,
+            options
+          );
+          const additionnalDataVideo = await axios.get(
+            `https://api.themoviedb.org/3/movie/${idMovie}/videos`,
+            options
+          );
+          const detailsResponse = await axios.get(
+            `https://api.themoviedb.org/3/movie/${idMovie}/images`,
+            options
+          );
+          console.log(
+            "additionnalDataVideo",
+            additionnalDataVideo.data.results[0].key
+          );
+          const responseIds = trendingResponse.data;
+          const finalData = {
+            id: responseIds.id,
+            title: responseIds.title,
+            original_title: responseIds.original_title,
+            backdrop_path: responseIds.backdrop_path,
+            overview: responseIds.overview,
+            video: additionnalDataVideo.data.results[0].key,
+            genres: responseIds.genres,
+            note: responseIds.vote_average,
+            images: detailsResponse.data.backdrops,
+          };
+          console.log("finalData", finalData);
+          setMoviesData([finalData]);
+          document.body.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${finalData.backdrop_path}`;
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-      } catch (error) {
-        console.log(error);
-      }
-    };
+      fetchData();
+    }
+  }, [idMovie]); // Dépend de idMovie
 
-    fetchData();
-
-    // Cleanup function to cancel any pending requests if the component unmounts
-    return () => {};
-  }, []); // Empty dependency array to ensure this effect runs only once
   return (
     <>
-  {moviesData.length > 0 && (
-    <div className={styles["movie-container"]}>
-      <div className={styles["movie-container__content"]}>
-        <div className={styles["movie-container__header"]}>
-          <h1 className={styles["movie-container__title"]}>{moviesData[0].original_title}</h1>
-          <div className={`${styles["movie-container__details"]} stack`}>
-            <h2 className={styles["movie-container__subtitle"]}>{moviesData[0].title}</h2>
-            <h2 className={styles["movie-container__rating"]}>{moviesData[0].note}</h2>
-            <p className={styles["movie-container__description"]}>{moviesData[0].overview}</p>
-            <div className={styles["movie-container__genres"]}>
-              {moviesData[0].genres.map((genre, index) => (
-                <p className={styles["movie-container__genre"]} key={index}>{genre.name}</p>
+      {moviesData.length > 0 && (
+        <div className={styles["movie-container"]}>
+          <div className={styles["movie-container__content"]}>
+            <div className={styles["movie-container__header"]}>
+              <h1 className={styles["movie-container__title"]}>
+                {moviesData[0].original_title}
+              </h1>
+              <div className={`${styles["movie-container__details"]} stack`}>
+                <h2 className={styles["movie-container__subtitle"]}>
+                  {moviesData[0].title}
+                </h2>
+                <h2 className={styles["movie-container__rating"]}>
+                  {moviesData[0].note}
+                </h2>
+                <p className={styles["movie-container__description"]}>
+                  {moviesData[0].overview}
+                </p>
+                <div className={styles["movie-container__genres"]}>
+                  {moviesData[0].genres.map((genre, index) => (
+                    <p className={styles["movie-container__genre"]} key={index}>
+                      {genre.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div className={styles["movie-container__iframe-wrapper"]}>
+                <iframe
+                  src={`https://www.youtube.com/embed/${moviesData[0].video}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                  className={styles["movie-container__iframe"]}
+                ></iframe>
+              </div>
+            </div>
+            <div className={styles["movie-container__images"]}>
+              {moviesData[0].images.slice(0, 9).map((image, index) => (
+                <img
+                  key={index}
+                  className={styles["movie-container__image"]}
+                  src={`https://image.tmdb.org/t/p/original${image.file_path}`}
+                  alt={image.file_path}
+                />
               ))}
             </div>
           </div>
-          <div className={styles["movie-container__iframe-wrapper"]}>
-            <iframe
-              src={`https://www.youtube.com/embed/${moviesData[0].video}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-              className={styles["movie-container__iframe"]}
-            ></iframe>
-          </div>
         </div>
-        <div className={styles["movie-container__images"]}>
-          {moviesData[0].images.slice(0, 9).map((image, index) => (
-            <img
-              key={index}
-              className={styles["movie-container__image"]}
-              src={`https://image.tmdb.org/t/p/original${image.file_path}`}
-              alt={image.file_path}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  )}
-</>
-
+      )}
+    </>
   );
 }
